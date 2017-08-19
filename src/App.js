d6fd658b684e6eeb755a57f6f0549b9d29dbdb0e
Luís Miguel <quicksaver@gmail.com>
2017-08-19 18:37:02 +0100

    using props to say hello to different names

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import HelloWorld from './HelloWorld';
 const App = () => {
 	return (
 		<div className="App">
-			<HelloWorld/>
-			<HelloWorld/>
+			<HelloWorld name="Jim"/>
+			<HelloWorld name="Pam"/>
 		</div>
 	);
 };
